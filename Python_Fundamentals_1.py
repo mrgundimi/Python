776cec0e2fe9b323de8457947a07f3adf19ce583
michael.gundimi@envestnet.com <michael.gundimi@envestnet.com>
2025-03-20 00:35:56 +0530

    Python Fundamentals | 19/03/2025

---
+++ Python_Fundamentals_1.py
@@ -0,0 +1,29 @@
+# Variables, Printing, Snakecase
+current_year = 2025
+print(current_year)
+
+# To avoid change in variable value
+CONSTANT_VARIABLES = 123
+print(CONSTANT_VARIABLES)
+
+# Numbers in Python
+my_age = 36
+PI = 3.14
+
+# Order in which Python executes Mathematical Operations.(* / + -)
+order_execution = 3*2/1+3-2
+print(order_execution)
+
+# Division result is always float
+x = 8/3
+print(x)
+
+# Divide and remove value after decimal point
+y = 8//3
+print(y)
+
+# Calculate reminder of two numbers using modulas
+calculate_remainder = 5 % 2
+print(calculate_remainder)
+
+# 
