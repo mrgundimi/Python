fd00f2d3cd6635e452e40a0525d673816a07e050
michael.gundimi@envestnet.com <michael.gundimi@envestnet.com>
2025-03-20 22:26:05 +0530

    Python Fundamentals | formatting strings

--- Python_Fundamentals_1.py
+++ Python_Fundamentals_1.py
@@ -26,4 +26,34 @@ print(y)
 calculate_remainder = 5 % 2
 print(calculate_remainder)
 
-# 
+# Strings, Invert Quotes, Escaping, multiline strings/comments, f-string, string formatting, Adding Strings, Using str
+my_name = "Michael"
+print(my_name)
+
+print("she's a girl")
+print('That is a "common" noun')                # Invert Quotes
+
+print("That is just \"freaking\" amazing")      # Escaping
+
+greetings_everyone = f"My name is {my_name}"
+print(greetings_everyone)                       # f-string
+
+my_name = "Brian"
+
+greetings_everyone = "My name is {}"
+my_name = "Peter"
+print(greetings_everyone.format(my_name))       # format string are basically used when we want to re-use a template.
+
+x = """
+         Hi 
+      This is a
+ Multi-line String/Comment                      
+"""
+print(x)                                        # multiline string/comment
+                                                # f-string can be used in multi-line strings
+
+blessed_name = "Ruel"
+print("His Name is " + blessed_name)            # Adding Strings
+
+blessed_age = 4
+print("His Age is " + str(blessed_age))
